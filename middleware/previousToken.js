9d353366594207c306cc336a30185f40b70c5eb6
Muhammad Hamza <mh408800@gmail.com>
2024-01-12 21:13:40 +0500

    code clean

--- middleware/previousToken.js
+++ middleware/previousToken.js
@@ -1,14 +1,15 @@
 const Token = require('../models/blacklistModel');
 
 async function checkExistingToken(req, res, next) {
+    // getting the token from the headers
     const authorizationHeader = req.headers.authorization;
-
+    // if token is missing
     if (!authorizationHeader) {
         return res.status(401).json({ message: 'Unauthorized - Missing Authorization Header' });
     }
-
+    // destructuring the token
     const [, currentToken] = authorizationHeader.split(' ');
-
+    // invalid header format, passing in the wrong headers
     if (!currentToken) {
         return res.status(401).json({ message: 'Unauthorized - Invalid Authorization Header Format' });
     }
@@ -23,12 +24,12 @@ async function checkExistingToken(req, res, next) {
             // Deny access if the current token exists in the database
             return res.status(401).json({ message: 'Unauthorized - Token destroyed' });
         }
-
+        // if everything is fine then, move next();
         // Continue with the next middleware or route handler
         next();
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        res.status(500).json({ message: 'Internal Server Error', error: "error" });
     }
 }
 
