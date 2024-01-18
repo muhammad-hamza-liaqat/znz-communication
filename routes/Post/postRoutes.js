3090b107f591a1c526bfe293196da2e63a28d0ee
Muhammad Hamza <mh408800@gmail.com>
2024-01-18 21:15:52 +0500

    postModel done

--- routes/Post/postRoutes.js
+++ routes/Post/postRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const postRouter = express.Router();
-const { addingPost } = require("../../controller/Post/postController")
-postRouter.route("/add-post").post(addingPost);
+const { addingPost } = require("../../controller/Post/postController");
+const { upload } = require("../../middleware/multer");
+
+postRouter.route("/add-post").post(upload.single("image"),addingPost);
 
 
 module.exports = postRouter
