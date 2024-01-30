3b0d326a43c0e649007406256d5f762af8470891
Muhammad Hamza <mh408800@gmail.com>
2024-01-30 19:31:25 +0500

    likePost Model done

---
+++ models/likepostModel.js
@@ -0,0 +1,35 @@
+const {DataTypes} = require("sequelize");
+const sequelize = require("../database/connection");
+
+
+const postLikeModel = sequelize.define("likes",{
+    likeID:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    likeUserID:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    likePostID: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+})
+
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("likePostModel synchronized with the database(znz).");
+  })
+  .catch((error) => {
+    console.error("Error synchronizing likePostModel", error);
+});
+
+
+module.exports = postLikeModel;
