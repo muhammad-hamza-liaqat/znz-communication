613488d305f4b08301036d131c17fee802a7f5a0
Muhammad Hamza <mh408800@gmail.com>
2024-01-30 20:08:53 +0500

    likePost controller added

--- controller/Like/likeController.js
+++ controller/Like/likeController.js
@@ -1,5 +1,54 @@
+const userModel = require("../../models/userModel");
+const postModel = require("../../models/postModel");
+const likePostModel = require("../../models/likepostModel");
+
+
+
 const likePost = async (req, res) => {
-  res.end("hello from like post");
+  const { likePostID } = req.body;
+  likeUserEmail = req.userEmail;
+  if (!likePostID || !likeUserEmail) {
+    return res.status(400).json({
+      statusCode: 400,
+      message: "likePostID or likeUserEmail is missing",
+    });
+  }
+  try {
+    const userExist = await userModel.findOne({
+      where: { email: likeUserEmail },
+    });
+    if (!userExist) {
+      return res
+        .status(404)
+        .json({ statusCode: 404, message: "user not found" });
+    }
+
+    const postExist = await postModel.findOne({
+      where: { postID: likePostID },
+    });
+    if (!postExist) {
+      return res
+        .status(404)
+        .json({ statusCode: 404, message: "post not found" });
+    }
+    const addLike = await likePostModel.create({
+      likeUserID: likeUserEmail,
+      likePostID: likePostID,
+    });
+    console.log(`${likeUserEmail} liked post against ${postExist.postID}`);
+    return res
+      .status(200)
+      .json({ statusCode: 200, message: "liked the post", data: addLike });
+  } catch (error) {
+    console.log("internal server error-likePost Controller", error);
+    return res
+      .status(500)
+      .json({
+        statusCode: 500,
+        message: "internal server error",
+        error: error,
+      });
+  }
 };
 
 module.exports = { likePost };
--- routes/Like/likeRoutes.js
+++ routes/Like/likeRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const likeRoutes = express.Router();
 const { likePost } = require("../../controller/Like/likeController");
+const { checkJWT } = require("../../middleware/authenticationMiddleware");
 
-likeRoutes.route("/like-post").post(likePost);
+likeRoutes.route("/like-post").post(checkJWT,likePost);
 
 module.exports = likeRoutes;
